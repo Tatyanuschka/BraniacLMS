43c4487956af2a18d1ca5bd1b0ed78ce0ddcebb5
Tatyana Lysenko <tatyana_lysenko@mail.ru>
2023-02-22 13:00:32 +0300

    Lesson3: args in url

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
     re_path("index", views.MainPageView.as_view(), name="main_page"),
     re_path("news", views.NewsPageView.as_view(), name="news"),
+    path("news/<int:page>", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
     re_path("courses", views.CoursesPageView.as_view(), name="courses"),
     re_path("contacts", views.ContactsPageView.as_view(), name="contacts"),
     re_path("doc_site", views.DocSitePageView.as_view(), name="doc_site"),
