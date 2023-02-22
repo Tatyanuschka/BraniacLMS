b8879d30c6dc58d4d8553934bec1e22cd2f54e62
Tatyana Lysenko <tatyana_lysenko@mail.ru>
2023-02-22 12:35:25 +0300

    Lesson3: urls in templates

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,11 +6,11 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path("", views.MainPageView.as_view()),
-    re_path("index", views.MainPageView.as_view()),
-    re_path("news", views.NewsPageView.as_view()),
-    re_path("courses", views.CoursesPageView.as_view()),
-    re_path("contacts", views.ContactsPageView.as_view()),
-    re_path("doc_site", views.DocSitePageView.as_view()),
-    re_path("login", views.LoginPageView.as_view()),
+    path("", views.MainPageView.as_view(), name="main_page"),
+    re_path("index", views.MainPageView.as_view(), name="main_page"),
+    re_path("news", views.NewsPageView.as_view(), name="news"),
+    re_path("courses", views.CoursesPageView.as_view(), name="courses"),
+    re_path("contacts", views.ContactsPageView.as_view(), name="contacts"),
+    re_path("doc_site", views.DocSitePageView.as_view(), name="doc_site"),
+    re_path("login", views.LoginPageView.as_view(), name="login"),
 ]
