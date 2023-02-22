a809cfd586cb949cadb940472437966c75af58d0
Tatyana Lysenko <tatyana_lysenko@mail.ru>
2023-02-22 10:31:39 +0300

    Lesson3: Tag with

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from mainapp import views
 from mainapp.apps import MainappConfig
@@ -7,9 +7,10 @@ app_name = MainappConfig.name
 
 urlpatterns = [
     path("", views.MainPageView.as_view()),
-    path("news/", views.NewsPageView.as_view()),
-    path("courses/", views.CoursesPageView.as_view()),
-    path("contacts/", views.ContactsPageView.as_view()),
-    path("doc_site/", views.DocSitePageView.as_view()),
-    path("login/", views.LoginPageView.as_view()),
+    re_path("index", views.MainPageView.as_view()),
+    re_path("news", views.NewsPageView.as_view()),
+    re_path("courses", views.CoursesPageView.as_view()),
+    re_path("contacts", views.ContactsPageView.as_view()),
+    re_path("doc_site", views.DocSitePageView.as_view()),
+    re_path("login", views.LoginPageView.as_view()),
 ]
