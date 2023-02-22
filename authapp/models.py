8de3f0387f0d88bc8881317a92123b6243313c5f
Tatyana Lysenko <tatyana_lysenko@mail.ru>
2023-02-23 00:27:03 +0300

    Lesson5: new config and migration

--- authapp/models.py
+++ authapp/models.py
@@ -21,28 +21,38 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     username_validator = ASCIIUsernameValidator()
 
     username = models.CharField(
-        _("username"), 
+        _("username"),
         max_length=150,
         unique=True,
-        help_text = _(
-        "Required. 150 charakters of fewer. Letters, digits and @/ ./+/-/_ only."
-        ),
-        vallidators = [username_validator],
-        error_messages = {
-            "unique": _("A user with this username already exists")
-        },
+        help_text=_("Required. 150 charakters of fewer. Letters, digits and @/ ./+/-/_ only."),
+        validators=[username_validator],
+        error_messages={"unique": _("A user with this username already exists")},
     )
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
     age = models.PositiveIntegerField(blank=True, null=True)
     avatar = models.ImageField(upload_to=users_avatars_path, blank=True, null=True)
-    email = models.CharField(_("email address"), max_length=256, unique=True, error_messages={
-            "unique": = _("A user with this email address already exists"),},)
-    is_staff = models.BooleanField(_("staff status"), default=False, help_text=_(
-        "Designates whether the user can log into this admin site."),)
-    is_active = models.BooleanField(_("active"), default=True, help_text=_(
-        "Designates whether this user should be treated as active. \
-        Unselect this instead of deleting accounts."), )
+    email = models.CharField(
+        _("email address"),
+        max_length=256,
+        unique=True,
+        error_messages={
+            "unique": _("A user with this email address already exists"),
+        },
+    )
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. \
+        Unselect this instead of deleting accounts."
+        ),
+    )
     date_joined = models.DateTimeField(_("date joined"), auto_now_add=True)
 
     objects = UserManager()
@@ -61,14 +71,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def get_full_name(self):
         full_name = "%s %s" % (self.first_name, self.last_name)
         return full_name.strip()
-    
+
     def get_short_name(self):
         return self.first_name
-    
+
     def email_user(self, subject, message, from_email=None, **kwargs):
         # send an email to this user
         send_mail(subject, message, from_email, [self.email], **kwargs)
-        
-
-    
-
